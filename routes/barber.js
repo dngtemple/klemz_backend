b6856f1f52508e35c4e498d112e5af20bff4bc7b
Clinton Templeton <donnahaytempleton1230@gmail.com>
2025-03-30 21:59:40 +0000

    adding a route to get all barbers without verification

--- routes/barber.js
+++ routes/barber.js
@@ -14,5 +14,14 @@ router.get('/barbers',verifyToken, async (req, res) => {
   }
 });
 
+router.get('/barbers/all', async (req, res) => {
+  try {
+    const barbers = await barberSchema.find(); 
+    res.status(200).json(barbers); 
+  } catch (error) {
+    res.status(500).json({ message: 'Server error, unable to fetch barbers' });
+  }
+});
+
 
 module.exports = router;
